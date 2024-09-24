ba03c42eb08cdebe11f62f0ddaac1041a76397d9
shtrikh17 <ilia.shilov@yandex.ru>
2024-09-24 15:48:29 +0300

    fix index

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,18 @@
-import {Button} from "./Button/Button";
-import {TableRow, TableHeader} from "./Table/Table";
-import {Pagination} from "./Pagination/Pagination";
-import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "./Modal/Modal";
-import {Breadcrumbs, BreadcrumbsItem } from "./Breadcrumbs/Breadcrumbs";
-import {Popover, PopoverContent, PopoverTrigger} from "./Popover/Popover";
-import { IconButton } from "./Button/IconButton";
-import { DeleteButton } from "./Button/DeleteButton";
-import {Input} from "./Input/Input";
-import {Chip} from "./Chip/Chip";
-import {Dropdown} from "./Dropdown/Dropdown"
-import {Tabs} from "./Tabs/Tabs"
-import { TextArea } from "./Input/Textarea";
-import { ChipInput } from "./Input/ChipInput";
-import {NotificationProvider, useNotifications} from "./Notifications/NotificationContext";
-import { MultiSelect, ChipSearchMultiSelect } from "./Select/MultiSelect";
-import { AutoComplete } from "./Autocomplete/AutoComplete";
-import { Switch } from "./Switch/Switch";
-import { Checkbox } from "./Checkbox/Checkbox";
-import { Radio } from "./Radio/Radio";
-import { PopConfirm } from "./Popover/PopConfirm";
-import { ChipList } from "./ChipList/ChipList";
-import { VerticalSteps } from "./VerticalSteps/VerticalSteps";
-import { TreeTable } from "./TreeTable/TreeTable";
-import { IconButtonLucide } from "./Button/IconButtonLucide";
-import { UploadButton } from "./Button/UploadButton";
-import {Calendar} from "./Calendar/Calendar"
-import {ImageGalleryCardBar} from "./ImageGallery/ImageGalleryCardBar";
-import {ImageGalleryCardCaption} from "./ImageGallery/ImageGalleryCardCaption";
-import {ScotchMenu} from "./Menu/ScotchMenu"
-import {Tooltip} from "./Tooltip/Tooltip"
-import { TextEditor } from "./TextEditor/TextEditor";
-import {ScotchRecords2} from "./Records2/Records2";
-import { TextEditorMulti } from "./TextEditorMulti/TextEditorMulti";
-import { DeleteButtonLucide } from "./Button/DeleteButtonLucide";
+import {AutoComplete} from "./Autocomplete/AutoComplete"
+import {ImageGallery} from "./ImageGallery/ImageGallery"
+import {ImageGalleryCard} from "./ImageGallery/ImageGalleryCard"
+import {ImageGalleryModal} from "./ImageGallery/ImageGalleryModal"
+import {SimpleSelect} from "./Select/SimpleSelect/SimpleSelect"
+import {MultiSelect} from "./Select/MultiSelect/MultiSelect"
+import {SearchSelect} from "./Select/SearchSelect/SearchSelect"
+import {SearchMultiSelect} from "./Select/SearchMultiSelect/SearchMultiSelect"
+import {Chip} from "./Chips/Chip/Chip"
+import {ChipList} from "./Chips/ChipList/ChipList"
+import {StringChipsInput} from "./Chips/StringChipsInput/StringChipsInput"
 
 export {
-    Button, DeleteButton, IconButton, IconButtonLucide, UploadButton, DeleteButtonLucide,
-    TableRow, TableHeader,
-    Modal, ModalHeader, ModalFooter, ModalBody, ModalContent,
-    Breadcrumbs, BreadcrumbsItem,
-    Pagination,
-    Popover, PopoverContent, PopoverTrigger, PopConfirm,
-    Input, TextArea, ChipInput,
-    Chip,
-    NotificationProvider, useNotifications,
-    Dropdown,
-    MultiSelect,
     AutoComplete,
-    Switch,
-    Tabs,
-    Checkbox,
-    Radio,
-    ChipList,
-    VerticalSteps,
-    TreeTable,
-    Calendar,
-    ImageGalleryList, ImageGalleryCard, ImageGalleryCardCaption, ImageGalleryCardBar,
-    Tooltip,
-    TextEditor,
-    ScotchMenu,
-    ScotchRecords2,
-    ChipSearchMultiSelect,
-    TextEditorMulti
+    ImageGallery, ImageGalleryModal, ImageGalleryCard,
+    SearchSelect, SimpleSelect, MultiSelect, SearchMultiSelect,
+    Chip, ChipList, StringChipsInput
 }
